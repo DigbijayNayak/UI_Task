852d3c235f87d7883f7fea9f9085330507144c92
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-06-29 14:42:01 +0530

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ebike-task1',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
